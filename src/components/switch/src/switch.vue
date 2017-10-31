7ed9bc1a76219799adc8a73f29910ff029fc09a1
jixuecong@lvwan.com <jixuecong@lvwan.com>
2017-10-31 17:06:02 +0800

    new component

---
+++ src/components/switch/src/switch.vue
@@ -0,0 +1,20 @@
+<template>
+    <div :class="computedClass">
+        <input type="checkbox" :name="name" class="am-switch-input">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'am-switch',
+        props: {
+
+        },
+        computed: {
+            computedClass() {
+
+            }
+        }
+    }
+</script>
