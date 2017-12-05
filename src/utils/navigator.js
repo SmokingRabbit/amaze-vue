806aa1a61bae69d6a737ed9f91c0f143d06ebdaa
jixuecong@lvwan.com <jixuecong@lvwan.com>
2017-12-05 18:24:23 +0800

    version@1.0.29

--- src/utils/navigator.js
+++ src/utils/navigator.js
@@ -1,13 +1,13 @@
 export function detectMobile() {
-    if(
+    if (
         navigator.userAgent.match(/Android/i) ||
-        navigator.userAgent.match(/webOS/i)  ||
+        navigator.userAgent.match(/webOS/i) ||
         navigator.userAgent.match(/iPhone/i) ||
         navigator.userAgent.match(/iPad/i) ||
         navigator.userAgent.match(/iPod/i) ||
         navigator.userAgent.match(/BlackBerry/i) ||
         navigator.userAgent.match(/Windows Phone/i)
-    ){
+    ) {
         return true;
     }
     else {
