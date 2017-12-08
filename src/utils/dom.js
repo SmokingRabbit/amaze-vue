db04797632a45274325769403ec75cfc6da10789
jixuecong@lvwan.com <jixuecong@lvwan.com>
2017-12-08 20:26:28 +0800

    update: 去掉utils dom检测

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,16 +1,8 @@
-function eleCheck(el) {
-    if (!(el instanceof HTMLElement)) {
-        console.error('arg[0] is not a HTMLElement');
-    }
-}
-
 export function on(el, event, func) {
-    eleCheck(el);
     el.addEventListener(event, func, false);
 }
 
 export function off(el, event, func) {
-    eleCheck(el);
     el.removeEventListener(event, func);
 }
 
